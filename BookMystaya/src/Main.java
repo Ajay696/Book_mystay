79d03a9fa873bfb4b35a662f9714c99882dcd707
Ajay696 <ajaysrini6@gmail.com>
2026-03-11 08:35:23 +0530

    .

---
+++ BookMystaya/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking Management System v1.0");
+        System.out.println("=====================================");
+    }
+}
